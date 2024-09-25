bbdc65b5f0ed10cbb6751923aec2ba2cfb7b64ae
Aditya <adityamaurya0401@gmail.com>
2024-09-25 14:19:38 +0530

    third

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 const router=require("express").Router()
 const User=require("../models/user")
-const bcrypt=require("bcrypt")
+const bcrypt = require('bcryptjs');
+
 const jwt=require("jsonwebtoken")
 const {authenticateToken}=require("./userAuth")
 // Sign up
